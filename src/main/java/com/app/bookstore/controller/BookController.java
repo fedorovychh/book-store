7f9294173713b5320344644a17769933c4dc46ae
Valerii Fedorovych <valera.fedorovich.2000@gmail.com>
2023-12-05 15:05:23 +0200

    BookController: added annotation

--- src/main/java/com/app/bookstore/controller/BookController.java
+++ src/main/java/com/app/bookstore/controller/BookController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,7 +27,7 @@ public class BookController {
     }
 
     @PostMapping
-    public Book createBook(CreateBookRequestDto bookDto) {
+    public Book createBook(@RequestBody CreateBookRequestDto bookDto) {
         return bookService.save(bookDto);
     }
 
