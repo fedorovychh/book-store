63e01bc99779435b3d016ac18b3905a59a55be5d
Valerii Fedorovych <valera.fedorovich.2000@gmail.com>
2023-12-09 14:17:20 +0200

    added soft delete concept

--- src/main/java/com/app/bookstore/controller/BookController.java
+++ src/main/java/com/app/bookstore/controller/BookController.java
@@ -5,11 +5,14 @@ import com.app.bookstore.dto.CreateBookRequestDto;
 import com.app.bookstore.service.book.BookService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
@@ -33,4 +36,9 @@ public class BookController {
         return bookService.save(requestDto);
     }
 
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        bookService.deleteById(id);
+    }
 }
--- src/main/java/com/app/bookstore/service/book/BookServiceImpl.java
+++ src/main/java/com/app/bookstore/service/book/BookServiceImpl.java
@@ -37,4 +37,9 @@ public class BookServiceImpl implements BookService {
                 .map(bookMapper::toDto)
                 .toList();
     }
+
+    @Override
+    public void deleteById(Long id) {
+        bookRepository.deleteById(id);
+    }
 }
