a02e1e30ab5ca9957b2dae307063260a201ae2a2
Valerii Fedorovych <valera.fedorovich.2000@gmail.com>
2023-12-11 17:07:01 +0200

    deleted redudant import

--- src/main/java/com/app/bookstore/service/book/BookServiceImpl.java
+++ src/main/java/com/app/bookstore/service/book/BookServiceImpl.java
@@ -9,7 +9,6 @@ import com.app.bookstore.repository.book.BookRepository;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
