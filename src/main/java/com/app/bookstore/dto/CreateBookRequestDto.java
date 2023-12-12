1b8ee48460edba0bbd28193d0a0c9221bf435336
Valerii Fedorovych <valera.fedorovich.2000@gmail.com>
2023-12-12 16:13:39 +0200

    changed annotation Min to Positive

--- src/main/java/com/app/bookstore/dto/CreateBookRequestDto.java
+++ src/main/java/com/app/bookstore/dto/CreateBookRequestDto.java
@@ -1,7 +1,7 @@
 package com.app.bookstore.dto;
 
-import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import java.math.BigDecimal;
 import lombok.Data;
 
@@ -14,7 +14,7 @@ public class CreateBookRequestDto {
     @NotNull
     private String isbn;
     @NotNull
-    @Min(0)
+    @Positive
     private BigDecimal price;
     private String description;
     private String coverImage;
