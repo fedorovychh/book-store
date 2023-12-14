b82a1b7eaad2d278658d7caa1ea671925f94069c
Valerii Fedorovych <valera.fedorovich.2000@gmail.com>
2023-12-14 14:12:16 +0200

    added size annotations

--- src/main/java/com/app/bookstore/dto/CreateBookRequestDto.java
+++ src/main/java/com/app/bookstore/dto/CreateBookRequestDto.java
@@ -2,16 +2,19 @@ package com.app.bookstore.dto;
 
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 import java.math.BigDecimal;
 import lombok.Data;
 
 @Data
 public class CreateBookRequestDto {
     @NotNull
+    @Size(min = 4, max = 20, message = "Title length should be between 4 and 20")
     private String title;
     @NotNull
     private String author;
     @NotNull
+    @Size(min = 13, max = 13, message = "ISBN length should be 13")
     private String isbn;
     @NotNull
     @Positive
