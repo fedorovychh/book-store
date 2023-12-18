bd85e069740d167b7d10e80d717318d9994a26fe
Valerii Fedorovych <valera.fedorovich.2000@gmail.com>
2023-12-18 16:38:10 +0200

    modified UserRepository

--- src/main/java/com/app/bookstore/repository/user/UserRepository.java
+++ src/main/java/com/app/bookstore/repository/user/UserRepository.java
@@ -2,10 +2,11 @@ package com.app.bookstore.repository.user;
 
 
 import com.app.bookstore.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
 }
