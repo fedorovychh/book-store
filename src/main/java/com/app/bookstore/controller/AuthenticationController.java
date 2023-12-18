e9495eddb2f7071e1cb868bc88fa47ed9e0ac791
Valerii Fedorovych <valera.fedorovich.2000@gmail.com>
2023-12-18 16:22:37 +0200

    added AuthenticationController and RegistrationException

---
+++ src/main/java/com/app/bookstore/controller/AuthenticationController.java
@@ -0,0 +1,19 @@
+package com.app.bookstore.controller;
+
+import com.app.bookstore.dto.UserRegistrationRequestDto;
+import com.app.bookstore.dto.UserResponseDto;
+import com.app.bookstore.exception.RegistrationException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(value = "/auth")
+public class AuthenticationController {
+    @PostMapping(value = "/registration ")
+    public UserResponseDto register(UserRegistrationRequestDto requestDto) throws RegistrationException {
+        return null;
+    }
+}
--- src/main/java/com/app/bookstore/dto/UserResponseDto.java
+++ src/main/java/com/app/bookstore/dto/UserResponseDto.java
@@ -3,7 +3,7 @@ package com.app.bookstore.dto;
 import lombok.Data;
 
 @Data
-public class UserDto {
+public class UserResponseDto {
     private Long id;
     private String email;
     private String firstName;
