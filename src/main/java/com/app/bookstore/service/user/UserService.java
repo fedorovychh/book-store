c47c0f68ca5fa9d2958183ab8198e5d1f57004e8
Valerii Fedorovych <valera.fedorovich.2000@gmail.com>
2023-12-18 16:46:01 +0200

    UserServiceImpl realized

--- src/main/java/com/app/bookstore/service/user/UserService.java
+++ src/main/java/com/app/bookstore/service/user/UserService.java
@@ -2,7 +2,8 @@ package com.app.bookstore.service.user;
 
 import com.app.bookstore.dto.UserRegistrationRequestDto;
 import com.app.bookstore.dto.UserResponseDto;
+import com.app.bookstore.exception.RegistrationException;
 
 public interface UserService {
-    UserResponseDto register(UserRegistrationRequestDto requestDto);
+    UserResponseDto register(UserRegistrationRequestDto requestDto) throws RegistrationException;
 }
--- src/main/java/com/app/bookstore/service/user/UserServiceImpl.java
+++ src/main/java/com/app/bookstore/service/user/UserServiceImpl.java
@@ -2,6 +2,9 @@ package com.app.bookstore.service.user;
 
 import com.app.bookstore.dto.UserRegistrationRequestDto;
 import com.app.bookstore.dto.UserResponseDto;
+import com.app.bookstore.exception.RegistrationException;
+import com.app.bookstore.mapper.UserMapper;
+import com.app.bookstore.model.User;
 import com.app.bookstore.repository.user.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -10,9 +13,14 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
+    private final UserMapper userMapper;
 
     @Override
-    public UserResponseDto register(UserRegistrationRequestDto requestDto) {
-        return null;
+    public UserResponseDto register(UserRegistrationRequestDto requestDto) throws RegistrationException {
+        if (userRepository.findByEmail(requestDto.getEmail()).isPresent()) {
+            throw new RegistrationException("Can't register user");
+        }
+        User user = userMapper.toUser(requestDto);
+        return userMapper.toDto(userRepository.save(user));
     }
 }
