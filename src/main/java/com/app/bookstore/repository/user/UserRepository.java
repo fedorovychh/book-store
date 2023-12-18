e503e49c325726de13f004e24927d4c58d73b0b9
Valerii Fedorovych <valera.fedorovich.2000@gmail.com>
2023-12-18 16:36:59 +0200

    added UserRepository and its implemetnation

---
+++ src/main/java/com/app/bookstore/repository/user/UserRepository.java
@@ -0,0 +1,11 @@
+package com.app.bookstore.repository.user;
+
+
+import com.app.bookstore.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
--- src/main/java/com/app/bookstore/service/user/UserServiceImpl.java
+++ src/main/java/com/app/bookstore/service/user/UserServiceImpl.java
@@ -2,10 +2,15 @@ package com.app.bookstore.service.user;
 
 import com.app.bookstore.dto.UserRegistrationRequestDto;
 import com.app.bookstore.dto.UserResponseDto;
+import com.app.bookstore.repository.user.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
+    private final UserRepository userRepository;
+
     @Override
     public UserResponseDto register(UserRegistrationRequestDto requestDto) {
         return null;
