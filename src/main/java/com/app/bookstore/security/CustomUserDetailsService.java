7873a39203f8f34a43b6c9550a85d925b727c725
Valerii Fedorovych <valera.fedorovich.2000@gmail.com>
2023-12-19 16:11:25 +0200

    modified CustomUserDetailsService

--- src/main/java/com/app/bookstore/security/CustomUserDetailsService.java
+++ src/main/java/com/app/bookstore/security/CustomUserDetailsService.java
@@ -1,12 +1,22 @@
 package com.app.bookstore.security;
 
+import com.app.bookstore.model.User;
+import com.app.bookstore.repository.user.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+@Service
+@RequiredArgsConstructor
 public class CustomUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(
+                        () -> new UsernameNotFoundException("Can't find user by email %s"));
         return null;
     }
 }
