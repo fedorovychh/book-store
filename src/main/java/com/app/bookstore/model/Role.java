6857ce3873cb4341e87a95342754d12f5df6cf4d
Valerii Fedorovych <valera.fedorovich.2000@gmail.com>
2023-12-19 17:17:57 +0200

    added Role entity

---
+++ src/main/java/com/app/bookstore/model/Role.java
@@ -0,0 +1,20 @@
+package com.app.bookstore.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true, nullable = false)
+    private String name;
+}
