f9cf689baaf5128b7abf6ee8d7f7f2b432b9fe79
Valerii Fedorovych <valera.fedorovich.2000@gmail.com>
2023-12-19 17:32:36 +0200

    added field roles to User entity

--- src/main/java/com/app/bookstore/model/User.java
+++ src/main/java/com/app/bookstore/model/User.java
@@ -5,9 +5,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import java.util.Collection;
 import java.util.List;
+import java.util.Set;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
@@ -36,6 +41,11 @@ public class User implements UserDetails {
     @Column(nullable = false)
     private String lastName;
     private String shippingAddress;
+    @ManyToMany
+    @JoinTable(name = "users_roles",
+                joinColumns = @JoinColumn(name = "user_id"),
+                inverseJoinColumns = @JoinColumn(name ="role_id"))
+    private Set<Role> roles;
     @Column(nullable = false)
     private boolean isDeleted = false;
 
