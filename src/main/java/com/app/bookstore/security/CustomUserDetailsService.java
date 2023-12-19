cffe053da22ac07f45921ca38b589d22e2d6862a
Valerii Fedorovych <valera.fedorovich.2000@gmail.com>
2023-12-19 16:23:26 +0200

    fixed message format

--- src/main/java/com/app/bookstore/security/CustomUserDetailsService.java
+++ src/main/java/com/app/bookstore/security/CustomUserDetailsService.java
@@ -14,9 +14,10 @@ public class CustomUserDetailsService implements UserDetailsService {
     private final UserRepository userRepository;
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        User user = userRepository.findByEmail(email)
+        return userRepository.findByEmail(email)
                 .orElseThrow(
-                        () -> new UsernameNotFoundException("Can't find user by email %s"));
-        return null;
+                        () -> new UsernameNotFoundException(String
+                                .format("Can't find user by email %s", email))
+                );
     }
 }
