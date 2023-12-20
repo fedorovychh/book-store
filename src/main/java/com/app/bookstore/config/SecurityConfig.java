bfafb1366542b36f5eed89fa65ae2772912bb81c
Valerii Fedorovych <valera.fedorovich.2000@gmail.com>
2023-12-20 17:08:55 +0200

    realized JWT authentication

--- src/main/java/com/app/bookstore/config/SecurityConfig.java
+++ src/main/java/com/app/bookstore/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.app.bookstore.config;
 
+import com.app.bookstore.security.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,6 +14,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @RequiredArgsConstructor
@@ -23,7 +25,9 @@ public class SecurityConfig {
             "/error",
             "/swagger-ui/**"
     };
+
     private final UserDetailsService userDetailsService;
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
     @Bean
     public PasswordEncoder getPasswordEncoder() {
@@ -43,6 +47,8 @@ public class SecurityConfig {
                                 .authenticated()
                 )
                 .httpBasic(Customizer.withDefaults())
+                .addFilterBefore(jwtAuthenticationFilter,
+                        UsernamePasswordAuthenticationFilter.class)
                 .userDetailsService(userDetailsService)
                 .build();
     }
