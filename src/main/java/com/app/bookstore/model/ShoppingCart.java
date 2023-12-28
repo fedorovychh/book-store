44163dba14b8d3e1d583366ff30f77c56345e59f
Valerii Fedorovych <valera.fedorovich.2000@gmail.com>
2023-12-28 12:18:36 +0200

    changed liquibase changeset

--- src/main/java/com/app/bookstore/model/ShoppingCart.java
+++ src/main/java/com/app/bookstore/model/ShoppingCart.java
@@ -19,8 +19,8 @@ import org.hibernate.annotations.Where;
 @Entity
 @SQLDelete(sql = "UPDATE shopping_carts SET is_deleted = true WHERE id=?")
 @Where(clause = "is_deleted=false")
-@Data
 @Table(name = "shopping_carts")
+@Data
 public class ShoppingCart {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
