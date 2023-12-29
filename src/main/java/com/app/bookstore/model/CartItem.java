605ced4d54c7c04f9fc962679885b814846dde64
Valerii Fedorovych <valera.fedorovich.2000@gmail.com>
2023-12-29 09:58:28 +0200

    excluded equals hashcode

--- src/main/java/com/app/bookstore/model/CartItem.java
+++ src/main/java/com/app/bookstore/model/CartItem.java
@@ -9,6 +9,8 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.Where;
 
@@ -22,9 +24,13 @@ public class CartItem {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @ManyToOne
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
     @JoinColumn(name = "shopping_cart_id", nullable = false)
     private ShoppingCart shoppingCart;
     @ManyToOne
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
     @JoinColumn(nullable = false, name = "book_id")
     private Book book;
     @Column(nullable = false)
