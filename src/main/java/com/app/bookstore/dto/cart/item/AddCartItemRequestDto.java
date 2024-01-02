dcc37fab31218d8e38fac4fd206a28fead28d497
Valerii Fedorovych <valera.fedorovich.2000@gmail.com>
2024-01-02 12:20:14 +0200

    added constraints

--- src/main/java/com/app/bookstore/dto/cart/item/AddCartItemRequestDto.java
+++ src/main/java/com/app/bookstore/dto/cart/item/AddCartItemRequestDto.java
@@ -1,9 +1,12 @@
 package com.app.bookstore.dto.cart.item;
 
+import jakarta.validation.constraints.Positive;
 import lombok.Data;
 
 @Data
 public class AddCartItemRequestDto {
+    @Positive
     private Long bookId;
+    @Positive
     private Integer quantity;
 }
