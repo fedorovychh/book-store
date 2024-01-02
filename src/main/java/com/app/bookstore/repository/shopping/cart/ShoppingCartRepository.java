5480907d4b25fc260cdbe2aceba22cff281bf526
Valerii Fedorovych <valera.fedorovich.2000@gmail.com>
2024-01-02 12:41:40 +0200

    changed refactored shopping cart service

--- src/main/java/com/app/bookstore/repository/shopping/cart/ShoppingCartRepository.java
+++ src/main/java/com/app/bookstore/repository/shopping/cart/ShoppingCartRepository.java
@@ -1,11 +1,10 @@
 package com.app.bookstore.repository.shopping.cart;
 
 import com.app.bookstore.model.ShoppingCart;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-
-import java.util.Optional;
 
 @Repository
 public interface ShoppingCartRepository extends JpaRepository<ShoppingCart, Long> {
