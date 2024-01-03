9d7a0dd430b54594cf1f12b500597cd0c761a47d
Valerii Fedorovych <valera.fedorovich.2000@gmail.com>
2024-01-03 12:14:33 +0200

    stable version

---
+++ src/main/java/com/app/bookstore/dto/order/item/OrderItemResponseDto.java
@@ -0,0 +1,4 @@
+package com.app.bookstore.dto.order.item;
+
+public class OrderItemResponseDto {
+}
---
+++ src/main/java/com/app/bookstore/mapper/OrderItemMapper.java
@@ -0,0 +1,8 @@
+package com.app.bookstore.mapper;
+
+import com.app.bookstore.config.MapperConfig;
+import org.mapstruct.Mapper;
+
+@Mapper(config = MapperConfig.class)
+public interface OrderItemMapper {
+}
