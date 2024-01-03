a2154af6a4175a2cd7f4e8e090aa61ceed8d32c4
Valerii Fedorovych <valera.fedorovich.2000@gmail.com>
2024-01-03 11:27:58 +0200

    added OrderController with structure (no method realization)

---
+++ src/main/java/com/app/bookstore/controller/OrderController.java
@@ -0,0 +1,59 @@
+package com.app.bookstore.controller;
+
+import com.app.bookstore.dto.order.OrderRequestDto;
+import com.app.bookstore.dto.order.OrderResponseDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(value = "/orders")
+@Tag(name = "Order management", description = "Endpoints for managing orders")
+public class OrderController {
+
+    @PreAuthorize("hasRole('ROLE_USER')")
+    @PostMapping
+    @Operation(summary = "Place order", description = "Place order")
+    public OrderResponseDto placeOrder(@RequestBody OrderRequestDto requestDto) {
+        return  null;
+    }
+
+    @PreAuthorize("hasRole('ROLE_USER')")
+    @GetMapping
+    @Operation(summary = "Get all orders", description = "Return all orders")
+    public OrderRequestDto getAll() {
+        return null;
+    }
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @PatchMapping("/{id}")
+    @Operation(summary = "Change order status", description = "Change order status")
+    public OrderResponseDto updateStatus(@PathVariable Long id, @RequestBody Object o) {
+        return  null;
+    }
+
+    @PreAuthorize("hasRole('ROLE_USER')")
+    @GetMapping("/{id}/items")
+    @Operation(
+            summary = "Get all items by order id",
+            description = "Retrieve all OrderItems for a specific order"
+    )
+    public Object getAllById(@PathVariable Long id) {
+        return null;
+    }
+
+    @PreAuthorize("hasRole('ROLE_USER')")
+    @GetMapping("/{id}/items/{itemId}")
+    public Object getItemById(@PathVariable Long id, @PathVariable Long itemId) {
+        return null;
+    }
+}
