f31039ac161268864ea87168785022c7f888bbc7
Valerii Fedorovych <valera.fedorovich.2000@gmail.com>
2024-01-03 17:50:11 +0200

    stable version (with bug)

--- src/main/java/com/app/bookstore/mapper/OrderItemMapper.java
+++ src/main/java/com/app/bookstore/mapper/OrderItemMapper.java
@@ -4,8 +4,10 @@ import com.app.bookstore.config.MapperConfig;
 import com.app.bookstore.dto.order.item.OrderItemResponseDto;
 import com.app.bookstore.model.OrderItem;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(config = MapperConfig.class)
 public interface OrderItemMapper {
+    @Mapping(target = "bookId", source = "book.id")
     OrderItemResponseDto toDto(OrderItem orderItem);
 }
--- src/main/java/com/app/bookstore/mapper/OrderMapper.java
+++ src/main/java/com/app/bookstore/mapper/OrderMapper.java
@@ -5,9 +5,11 @@ import com.app.bookstore.dto.order.OrderRequestDto;
 import com.app.bookstore.dto.order.OrderResponseDto;
 import com.app.bookstore.model.Order;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
-@Mapper(config = MapperConfig.class)
+@Mapper(config = MapperConfig.class, uses = OrderItemMapper.class)
 public interface OrderMapper {
+    @Mapping(target = "userId", source = "user.id")
     OrderResponseDto toDto(Order order);
 
     Order toOrder(OrderRequestDto requestDto);
