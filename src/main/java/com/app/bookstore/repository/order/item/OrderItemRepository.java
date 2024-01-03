15647e46fb0a3f1cc7bb89df7625d3079813ca5d
Valerii Fedorovych <valera.fedorovich.2000@gmail.com>
2024-01-03 12:59:34 +0200

    updated OrderItemRepository

--- src/main/java/com/app/bookstore/repository/order/item/OrderItemRepository.java
+++ src/main/java/com/app/bookstore/repository/order/item/OrderItemRepository.java
@@ -4,7 +4,9 @@ import com.app.bookstore.model.OrderItem;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
     @Query("SELECT oi FROM OrderItem oi "
             + "JOIN FETCH oi.order "
